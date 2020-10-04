09be84207693c665007c444a037b281ba25e64d5
prajin khadka <prawjeenkhadka@gmail.com>
2020-10-04 12:20:32 +0545

    Net centric basic project - Restaurant management

---
+++ RestaurantManagement/RestaurantManagement/Controllers/PieController.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RestaurantManagement.Models;
+using RestaurantManagement.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RestaurantManagement.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        // GET: /<controller>/
+        public IActionResult List()
+        {
+            //ViewBag.CurrentCategory = "Cheese cakes";
+
+            //return View(_pieRepository.AllPies);
+            PiesListViewModel piesListViewModel = new PiesListViewModel();
+            piesListViewModel.Pies = _pieRepository.AllPies;
+
+            piesListViewModel.CurrentCategory = "Cheese cakes";
+            return View(piesListViewModel);
+        }
+    }
+}
